3d5b2b93ceb63c0cfdce448b651b8e3d45f38918
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-10-20 17:48:38 +0100

    removed framer m

--- components/General/Intro/Intro.tsx
+++ components/General/Intro/Intro.tsx
@@ -1,17 +1,11 @@
 import { useRouter } from "next/router"
-import { AnimatePresence, motion } from "framer-motion"
-import { FC, useState } from "react"
+import { FC } from "react"
 import classes from "./Intro.module.css"
 
 const Intro: FC = () => {
 	const router = useRouter()
 	return (
-		<motion.div
-			initial={{ x: "400%", opacity: 0 }}
-			animate={{ x: 0, opacity: 1 }}
-			transition={{ type: "tween", delay: 0.3, duration: 0.1 }}
-			className={classes.main}
-		>
+		<div className={classes.main}>
 			<h1>Create instant, online, real-time polls</h1>
 			<button
 				className="btn"
@@ -25,7 +19,7 @@ const Intro: FC = () => {
 			<p>
 				No registration required. It's 100% free and takes less than a minute.
 			</p>
-		</motion.div>
+		</div>
 	)
 }
 
