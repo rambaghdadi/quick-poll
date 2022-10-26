32383a6798497621d68bedf4dae0207b082c535c
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-10-26 14:12:45 +0100

    added different animation for homepage

--- components/General/Intro/Intro.tsx
+++ components/General/Intro/Intro.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import { FC, useEffect, useRef, useState } from "react"
+import { FC, useEffect, useRef } from "react"
 import classes from "./Intro.module.css"
 
 const Intro: FC = () => {
@@ -9,29 +9,50 @@ const Intro: FC = () => {
 	const wordsToLoop = ["fun", "work", "free"]
 
 	const sleep = (m: number) => new Promise((r) => setTimeout(r, m))
-	async function loopThroughLetters(word: string) {
-		let bg = "#1e1e1ec0"
-		let currentColor = "white"
-		let newColor = "#333"
-		if (
-			window.matchMedia &&
-			window.matchMedia("(prefers-color-scheme: dark)").matches
-		) {
-			bg = "rgba(255, 255, 255, 0.863)"
-			currentColor = "#333"
-			newColor = "white"
-		}
-		await sleep(2000)
-		wordInHeadline.current!.style.backgroundColor = bg
-		wordInHeadline.current!.style.color = currentColor
-		await sleep(800)
-		//delete word
-		wordInHeadline.current!.innerHTML = ""
-		wordInHeadline.current!.style.backgroundColor = "transparent"
-		wordInHeadline.current!.style.color = newColor
-		await sleep(800)
 
-		//add new word
+	// async function loopThroughLetters(word: string) {
+	// 	let bg = "#1e1e1ec0"
+	// 	let currentColor = "white"
+	// 	let newColor = "#333"
+	// 	if (
+	// 		window.matchMedia &&
+	// 		window.matchMedia("(prefers-color-scheme: dark)").matches
+	// 	) {
+	// 		bg = "rgba(255, 255, 255, 0.863)"
+	// 		currentColor = "#333"
+	// 		newColor = "white"
+	// 	}
+	// 	await sleep(2000)
+	// 	wordInHeadline.current!.style.backgroundColor = bg
+	// 	wordInHeadline.current!.style.color = currentColor
+	// 	await sleep(800)
+	// 	//delete word
+	// 	wordInHeadline.current!.innerHTML = ""
+	// 	wordInHeadline.current!.style.backgroundColor = "transparent"
+	// 	wordInHeadline.current!.style.color = newColor
+	// 	await sleep(800)
+
+	// 	//add new word
+	// 	for (let letter of word) {
+	// 		wordInHeadline.current!.innerHTML += letter
+	// 		await sleep(200)
+	// 	}
+	// }
+
+	async function loopThroughLetters(word: string) {
+		await sleep(1000)
+		//delete letters
+		const wordLength = wordInHeadline.current!.innerHTML.length
+		let i = 0
+		while (i < wordLength) {
+			await sleep(100)
+			wordInHeadline.current!.innerHTML =
+				wordInHeadline.current!.innerHTML.slice(0, -1)
+			i++
+		}
+
+		// add new word
+		await sleep(1000)
 		for (let letter of word) {
 			wordInHeadline.current!.innerHTML += letter
 			await sleep(200)
@@ -54,7 +75,7 @@ const Intro: FC = () => {
 	return (
 		<div className={classes.main}>
 			<h1>
-				Create instant, online, real-time polls for{" "}
+				Create quick, real-time polls for{" "}
 				<span ref={wordInHeadline} className={classes.changingText}>
 					free
 				</span>
@@ -68,9 +89,7 @@ const Intro: FC = () => {
 			>
 				Create New Poll
 			</button>
-			<p>
-				No registration required. It's 100% free and takes less than a minute.
-			</p>
+			<p>No registration required.</p>
 		</div>
 	)
 }
