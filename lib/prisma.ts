e2758da732840682fd06f8e20131b53d2709bb6d
Ram Baghdadi <ram_baghdadi@hotmail.com>
2023-05-02 13:48:26 +0100

    updated env files and vercel file to update db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,10 @@
-import { PrismaClient } from "@prisma/client"
-declare let global: { prisma: PrismaClient }
-
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === "production") {
-	prisma = new PrismaClient()
-} else {
-	if (!global.prisma) {
-		global.prisma = new PrismaClient()
-	}
-	prisma = global.prisma
+import {PrismaClient} from "@prisma/client"
+declare global {
+  var prisma: PrismaClient | undefined
 }
+
+const prisma = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma
 
 export default prisma
